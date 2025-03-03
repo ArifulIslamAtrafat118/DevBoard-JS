321b928fd6c19fa5597baeeb403045c1f10a6995
ArifulIslamAtrafat118 <arafatarifulislam611@gmail.com>
2025-03-03 22:36:40 +0600

    activity log messege done

--- script/compleate-btn.js
+++ script/compleate-btn.js
@@ -5,6 +5,7 @@ let buttons = document.getElementsByClassName("btn-compleate");
 for(const button of buttons){
     button.addEventListener('click', function(){
         alert("Board Updated Successfully");
+
         let activityMessage = document.getElementById("activity-message");
         let newMessageDiv = document.createElement('div');
         newMessageDiv.classList.add("activity-message", "bg-[#F4F7FF]", "rounded-lg", "px-4", "py-3");
@@ -12,16 +13,24 @@ for(const button of buttons){
                                 id="" class="">${getCurrentTime()}</span></p>`;
         let activityContainer = document.getElementById("activity-container");
         activityContainer.appendChild(newMessageDiv);
+
         // get  headline 
         let parentDiv = this.closest(".task-card-parent-div");
         const taskCardHeadline = parentDiv.querySelector("h2");
         const taskCardHeadlineText = taskCardHeadline.innerText;
+
         //set headline
-        const setTaskHeadline = document.getElementById("task-headline-target-span");
+        const setTaskHeadline = newMessageDiv.querySelector("#task-headline-target-span"); 
+        //note: newMessageDiv  create kora hoyce ..... querySelector newMessageDiv theke task-headline-target-span namok id'r element(span) ke khuje nea ase
+        //then otar bitore taskCardHeadlineText ke dukai dey.
         setTaskHeadline.innerText = taskCardHeadlineText; 
+
         //disable the button
         button.setAttribute("disabled", "true");
         button.classList.add("opacity-30");
         button.innerText = "Completed";
+        
+        //show the final alart
+        
     })
 }
--- script/toggleColor.js
+++ script/toggleColor.js
@@ -1,6 +1,6 @@
 document.getElementById("change-color-theme")
     .addEventListener('click', function() {
-        let colors= ["bg-red-50", "bg-blue-100", "bg-green-100", "bg-yellow-100", "bg-purple-100", "bg-orange-100", "bg-amber-100", "bg-lime-200", "bg-teal-100", "bg-cyan-100", "bg-sky-100", "bg-violet-300", "bg-pink-200"];
+        let colors= ["bg-red-50", "bg-blue-100", "bg-green-100", "bg-yellow-100", "bg-purple-100", "bg-orange-100", "bg-amber-100", "bg-lime-200", "bg-teal-100", "bg-cyan-100", "bg-sky-100", "bg-violet-200", "bg-pink-200", "bg-pink-50"];
         document.body.classList.remove(...colors);
         let randomColor = colors[Math.floor(Math.random() * colors.length)];
         document.body.classList.add(randomColor, "transition-colors" , "duration-500");
